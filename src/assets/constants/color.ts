df4db77582de70bc352ca92e0ca58c205a3ce614
Gabriel Davi <gdavi.gd@gmail.com>
2021-06-21 23:46:49 -0300

    feat: init the project and add styled components

---
+++ src/assets/constants/color.ts
@@ -0,0 +1,19 @@
+export const colors = {
+  black: "#000",
+  shadow: "#050206",
+  purple: "#835AFD",
+  danger: "#E73F5D",
+  gray: {
+    dark: "#737380",
+    medium: "#A8A8B3",
+    light: "#DBDCDD"
+  },
+  white: {
+    background: "#F8F8F8",
+    details: "#FEFEFE"
+  },
+  pink: {
+    dark: "#E559F9",
+    light: "#D67EE2"
+  },
+}
---
+++ src/assets/constants/fonts.ts
@@ -0,0 +1,7 @@
+export const fonts = {
+  title: {
+    fontWeight: 700,
+    fontFamily: 'Roboto',
+    fontSize: 24
+  }
+}
